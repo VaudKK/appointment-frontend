a320806497f2429c794d4fc199121d6f569e729f
VaudKK <vaudkeith@gmail.com>
2026-03-12 22:29:43 +0300

    debug: connectivity

--- lib/api/mail.ts
+++ lib/api/mail.ts
@@ -3,21 +3,9 @@
 
 import { SendMailRequest, SendMailResponse } from "@/lib/types"
 
-const getBaseUrl = () => {
-    const explicitBaseUrl =
-        process.env.NEXT_INTERNAL_APP_URL ||
-        process.env.NEXT_PUBLIC_APP_URL ||
-        process.env.BETTER_AUTH_URL ||
-        (process.env.RAILWAY_STATIC_URL ? `https://${process.env.RAILWAY_STATIC_URL}` : undefined) ||
-        (process.env.RAILWAY_PUBLIC_DOMAIN ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` : undefined) ||
-        (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined)
-
-    return (explicitBaseUrl ?? "http://localhost:3000").replace(/\/$/, "")
-}
-
 
 export async function sendEmail(sendMailRequest: SendMailRequest): Promise<SendMailResponse> {
-    const res = await fetch(`${getBaseUrl()}/api/mail/send`, {
+    const res = await fetch(`/api/mail/send`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
